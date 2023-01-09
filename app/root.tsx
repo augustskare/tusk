480dfc70e48174096f8f8287ce0b3c829e4d3362
August Skare <post@augustskare.no>
2023-01-09 20:42:20 +0100

    Fix iOS landscape styling

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export const links: LinksFunction = () => [
 export const meta: MetaFunction = () => ({
   title: "Tusk",
   charset: "utf-8",
-  viewport: "width=device-width,initial-scale=1",
+  viewport: "width=device-width,initial-scale=1,viewport-fit=cover",
 });
 
 export async function loader({ request }: LoaderArgs) {
